ca7cfc1860866d7ac7ccf91640fb968f7c27b989
Lok <lok@pg95.com>
2024-05-16 14:00:04 +0100

    writing test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,14 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
+    test('Unit Test', function(){
+        assert.equal(convertHandler.getNum('12 lbs'), '12', 'should correctly read a whole number input');
+        assert.equal(convertHandler.getNum('12.05 lbs'), '12.05', 'shuold correctly read a decimal number input');
+        assert.equal(convertHandler.getNum('2/5 lbs'), '0.4', 'should correctly read a fractional input');
+        assert.equal(convertHandler.getNum('2.5/10 lbs'), '0.25', 'should correctly read a fractional input with a decimal');
+        assert.throws(()=>{convertHandler.getNum('2/5/10 lbs')}, Error, 'should return an error on double-fraction');
 
+        
+
+    })
 });

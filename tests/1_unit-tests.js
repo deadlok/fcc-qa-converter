3a92e372cb2b14d4632cfda92e44a53a884cdb4f
Lok <lok@pg95.com>
2024-05-17 23:29:27 +0100

    fix unit test

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,25 +5,66 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
-    test('Unit Test', function(){
-        assert.equal(convertHandler.getNum('12 lbs'), '12', 'should correctly read a whole number input');
-        assert.equal(convertHandler.getNum('12.05 lbs'), '12.05', 'shuold correctly read a decimal number input');
+    test('should correctly read a whole number input', function(){
+        assert.equal(convertHandler.getNum('12 lbs'), '12', 'should return 12');
+    })
+    test('should correctly read a decimal number input', function(){
+        assert.equal(convertHandler.getNum('12.05 lbs'), '12.05', 'should return 12.05');
+    })
+
+    test('should correctly read a fractional input', function (){
         assert.equal(convertHandler.getNum('2/5 lbs'), '0.4', 'should correctly read a fractional input');
+    })
+
+    test('should correctly read a fractional input', function (){
         assert.equal(convertHandler.getNum('2.5/10 lbs'), '0.25', 'should correctly read a fractional input with a decimal');
+    })
+
+    test('should correctly return an error on a double-fraction', function (){
         assert.throws(()=>{convertHandler.getNum('2/5/10 lbs')}, Error, 'invalid number');
+    })
+
+    test('should correctly default to a numerical input of 1 when no numerical input is provided.', function (){
         assert.equal(convertHandler.getNum('lbs'), 1, 'default to a numerical input of 1');
+    })
+
+    test('should correctly read each valid input unit',function (){
         assert.equal(convertHandler.getUnit('1lbs'),'lbs','should correctly read each input unit');
+    })
+
+    test('should correctly return an error for an invalid input unit.',function (){
         assert.throws(()=>{convertHandler.getUnit('1xyz')}, Error, 'invalid unit');
+    })
+
+    test('should return the correct return unit for each valid input unit.',function (){
         assert.equal(convertHandler.getReturnUnit('lbs'),'kg','should correctly return unit');
+    })
+
+    test('should correctly return the spelled-out string unit for each valid input unit.',function (){
         assert.equal(convertHandler.spellOutUnit('lbs'),'pounds','should correctly return spelled-out unit');
-        
+    })
+
+    test('should correctly convert gal to L.',function (){        
         assert.equal(convertHandler.convert(5, 'gal'), 18.92705 ,'should correctly convert gal to L');
+    })
+ 
+    test('should correctly convert L to gal.',function (){        
         assert.equal(convertHandler.convert(5, 'L'), 1.32086 ,'should correctly convert gal to L');
+    })
 
+    test('should correctly convert mi to km.',function (){        
         assert.equal(convertHandler.convert(5, 'mi'), 8.0467 ,'should correctly convert gal to L');
-        assert.equal(convertHandler.convert(5, 'km'), 3.10686 ,'should correctly convert gal to L');
+    })
 
+    test('should correctly convert km to mi.',function (){        
+        assert.equal(convertHandler.convert(5, 'km'), 3.10686 ,'should correctly convert gal to L');
+    })
+
+    test('should correctly convert lbs to kg.',function (){        
         assert.equal(convertHandler.convert(5, 'lbs'), 2.26796 ,'should correctly convert gal to L');
+    })
+
+    test('should correctly convert kg to lbs.',function (){        
         assert.equal(convertHandler.convert(5, 'kg'), 11.02312 ,'should correctly convert gal to L');
     })
 });
